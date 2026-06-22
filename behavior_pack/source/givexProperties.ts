db665921ab9c6339ea5f1441513e48c585d7f636
Frogzalcoatl <saharholmes@gmail.com>
2026-06-22 16:51:26 -0400

    Began working on transferring itemProperties to a class. Going to take a break from this project now

---
+++ behavior_pack/source/givexProperties.ts
@@ -0,0 +1,20 @@
+import type { EnchantData, ItemDurability, SlotData } from "./types";
+
+export class GivexProperties {
+	public typeId: string;
+	public amount: number;
+	public lockMode?: string; // ItemLockMode,
+	public nameTag?: string;
+	// Must be less than the item's max durability, or the str "unbreakable" to for infinite durability
+	public durability?: ItemDurability;
+	// Dyeable component simply doesn't exist on vanilla items. (Bug tracker MCPE-237577 and MCPE-232617)
+	// public dye?: RGB;
+	public enchants?: EnchantData[];
+	public slot: SlotData | undefined;
+	public potionType?: string;
+	public arrowType?: string;
+	public bedColor?: string;
+	public keepOnDeath?: boolean;
+	public canPlaceOn?: string[];
+	public canDestroy?: string[];
+}
