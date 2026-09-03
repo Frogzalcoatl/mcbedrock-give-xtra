c335a55ad93fff8af1dd0c7fa2d0a923dcb085c6
Frogzalcoatl <saharholmes@gmail.com>
2026-09-02 23:27:34 -0400

    Fix junctions.bat path

--- junctions.bat
+++ junctions.bat
@@ -11,8 +11,8 @@ set /P "runCommands=Would you like to create pack junctions in the shared com.mo
 if /I "%runCommands%"=="y" (
 	echo Running commands...
 	:: %~dp0 represents directory of batch file
-	mklink /j "%appdata%\Minecraft Bedrock\Users\Shared\games\com.mojang\development_resource_packs\mcbedrock-give-xtra" "%~dp0resource_pack"
-	mklink /j "%appdata%\Minecraft Bedrock\Users\Shared\games\com.mojang\development_behavior_packs\mcbedrock-give-xtra" "%~dp0behavior_pack"
+	mklink /j "%appdata%\Minecraft Bedrock\Users\Shared\games\com.mojang\development_resource_packs\mcbedrock-give-xtra" "%~dp0resources"
+	mklink /j "%appdata%\Minecraft Bedrock\Users\Shared\games\com.mojang\development_behavior_packs\mcbedrock-give-xtra" "%~dp0behaviors"
 ) else (
 	echo Cancelled
 )
